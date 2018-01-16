b3b51c6a72b3d0dab1c98b5b958e62e9563e0ba1
Xayaseth Boudsady <boudsadyx@gmail.com>
2018-01-15 23:58:47 -0800

    Weathe printer app aded

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 // Problem: We need a simple way to look a a user's badege count and JavaScript points
 // Solution: Use Node.js to connect to Treehouse's API to get profile information to print out
 
- const profile = require('./profile');
+ const profile = require('./profilegi');
 
 const users= process.argv.slice(2);
 users.forEach(profile.get);
